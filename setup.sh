543f7ee720be7623288d9d64d8054ce34a8aeb64
Kan Fushihara <kan@debian.local>
2009-05-14 12:44:52 +0900

    editorとpagerの指定を追加 新環境で簡単に設定を適用するシェルスクリプトを入れてみる

---
+++ setup.sh
@@ -0,0 +1,5 @@
+~/project/dotfiles/zsh/.zshrc ~/.zshrc
+~/project/dotfiles/vim/.vimrc ~/.vimrc
+~/project/dotfiles/screen/.screenrc ~/.screenrc
+~/project/dotfiles/git/.gitconfig ~/.gitconfig
+~/project/dotfiles/git/.gitignore ~/.gitignore
