eb27b1df7d4ed810996a2e34ed875742e85e273b
Kan Fushihara <kan@debian.local>
2009-05-15 18:13:40 +0900

    bug fixed
    シェルスクリプトが動かないので修正
    .gitで名前を始めるとignoreとの絡みで面倒そうなので改名

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-~/project/dotfiles/zsh/.zshrc ~/.zshrc
-~/project/dotfiles/vim/.vimrc ~/.vimrc
-~/project/dotfiles/screen/.screenrc ~/.screenrc
-~/project/dotfiles/git/.gitconfig ~/.gitconfig
-~/project/dotfiles/git/.gitignore ~/.gitignore
+ln -s ~/project/dotfiles/zsh/.zshrc ~/.zshrc
+ln -s ~/project/dotfiles/vim/.vimrc ~/.vimrc
+ln -s ~/project/dotfiles/screen/.screenrc ~/.screenrc
+ln -s ~/project/dotfiles/git/gitconfig ~/.gitconfig
+ln -s ~/project/dotfiles/git/gitignore ~/.gitignore
