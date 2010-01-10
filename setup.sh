8590da71c4511574e681d067333e82d3b48d0dff
Kan Fushihara <kan@edf3.(none)>
2010-01-10 17:09:52 +0900

    ファイル名揃える

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 ln -s ~/project/dotfiles/zsh/.zshrc ~/.zshrc
-ln -s ~/project/dotfiles/zshenv ~/.zshenv
+ln -s ~/project/dotfiles/zsh/.zshenv ~/.zshenv
 ln -s ~/project/dotfiles/vim/.vimrc ~/.vimrc
 ln -s ~/project/dotfiles/screen/.screenrc ~/.screenrc
 ln -s ~/project/dotfiles/git/gitconfig ~/.gitconfig
