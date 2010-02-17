e8652472767dc706b83dfcf0850dbb29a2d5ce5f
Kan Fushihara <kan@onion.fushihara.home>
2010-02-18 01:23:16 +0900

    setting vim colorscheme

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 ln -s ~/project/dotfiles/zsh/.zshrc ~/.zshrc
 ln -s ~/project/dotfiles/zsh/.zshenv ~/.zshenv
 ln -s ~/project/dotfiles/vim/.vimrc ~/.vimrc
+ln -s ~/project/dotfiles/vim/.vim ~/.vim
 ln -s ~/project/dotfiles/screen/.screenrc ~/.screenrc
 ln -s ~/project/dotfiles/git/gitconfig ~/.gitconfig
 ln -s ~/project/dotfiles/git/gitignore ~/.gitignore
